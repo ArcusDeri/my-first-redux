9243ad081f89fd219391562b33024b61323b8fb8
Marcin Michniak <marcin.michniak@fortedigital.no>
2018-11-19 11:36:44 +0100

    Add basic dependencies and Home, About components.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './components/home/Home';
+import About from './components/about/About';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <BrowserRouter>
+      <div className="container-fluid">
+        <p>Header</p>
+        {this.props.children}
+        <Switch>
+          <Route path="/" exact component={ Home } />
+          <Route path="/about" component={ About } />
+        </Switch>
       </div>
+      </BrowserRouter>
     );
   }
 }
